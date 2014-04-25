ad9c44e63d6dfc7da7061c5bf8bf601e71362792
mrt168 <kurafuto16@gmail.com>
2014-04-25 11:44:04 +0900

    関係消去

--- GifMagazine/src/jp/createbox/gifmagazine/LoginActivity.java
+++ GifMagazine/src/jp/createbox/gifmagazine/LoginActivity.java
@@ -1,6 +1,5 @@
 package jp.createbox.gifmagazine;
 
-import jp.createbox.gifmagazine.flagment.LoginFragment;
 import android.support.v4.app.FragmentActivity;
 
 import android.os.Bundle;
@@ -8,28 +7,28 @@ import android.view.Menu;
 
 public class LoginActivity extends FragmentActivity {
 
-	LoginFragment loginFragment;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (savedInstanceState == null) {
-			// Add the fragment on initial activity setup
-			loginFragment = new LoginFragment();
-
-			getSupportFragmentManager().beginTransaction()
-					.add(android.R.id.content, loginFragment).commit();
-		} else {
-			// Or set the fragment from restored state info
-			loginFragment = (LoginFragment) getSupportFragmentManager()
-					.findFragmentById(android.R.id.content);
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+//	LoginFragment loginFragment;
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		if (savedInstanceState == null) {
+//			// Add the fragment on initial activity setup
+//			loginFragment = new LoginFragment();
+//
+//			getSupportFragmentManager().beginTransaction()
+//					.add(android.R.id.content, loginFragment).commit();
+//		} else {
+//			// Or set the fragment from restored state info
+//			loginFragment = (LoginFragment) getSupportFragmentManager()
+//					.findFragmentById(android.R.id.content);
+//		}
+//	}
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
 
 }
